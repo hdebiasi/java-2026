cf2069b8517498c2a4aa025e0501927e558d6784
Herculano De Biasi <02105018980@udesc.br>
2026-09-13 10:17:14 -0300

    decisão agora com entrada de dados (Scanner)

--- src/basico/Decisao.java
+++ src/basico/Decisao.java
@@ -1,8 +1,12 @@
 package basico;
 
+import java.util.Scanner;
+
 public class Decisao {
     public static void main(String[] args) {
-        int nota = 7;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite a nota entre 0 e 10: ");
+        int nota = entrada.nextInt();
 
         if (nota >= 7) {
             System.out.println("Passou direto!");
