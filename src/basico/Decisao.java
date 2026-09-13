6f427d327134b7bd6f6c42b9fe7f8456b76bb2cb
Herculano (Unoesc) <herculano.debiasi@unoesc.edu.br>
2026-09-13 11:17:28 -0300

    comentário de teste

--- src/basico/Decisao.java
+++ src/basico/Decisao.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Decisao {
     public static void main(String[] args) {
+        // Comentário de teste
         Scanner entrada = new Scanner(System.in);
         System.out.print("Digite a nota entre 0 e 10: ");
         int nota = entrada.nextInt();
